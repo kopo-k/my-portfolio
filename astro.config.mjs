8afe8e4165ba9cc9c33ca522f15131ccc872c2c4
こぽー <tora1025oishi@gmail.com>
2025-06-30 11:09:50 +0900

    ogp setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind'; // ← tailwindを使ってる場合
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://kopo-k.github.io/my-portfolio/",
+  base: "/my-portfolio/",
+  integrations: [tailwind()], // ← 必要な統合
+  outDir: "dist", // デフォルトだが明示してもOK
+});
