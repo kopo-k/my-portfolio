364f160daae2a9ea561604b5d7221f1d07c7b28c
こぽー <tora1025oishi@gmail.com>
2025-06-30 10:40:21 +0900

    add:Sending message function

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,21 @@ module.exports = {
   content: [
     './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
   ],
+  
   theme: {
     extend: {
+      animation: {
+        'pulse-slow': 'pulse-slow 2s infinite',
+        'spin-outer': 'rotate-outer 1.5s ease-in-out infinite',
+        'spin-inner': 'rotate-inner 1.2s ease-in-out infinite reverse',
+        'pulse-inner': 'inner-pulse 1.8s infinite reverse',
+      },
+      keyframes: {
+        'pulse-slow': {
+          '0%, 100%': { transform: 'scale(1)', opacity: '1' },
+          '50%': { transform: 'scale(1.1)', opacity: '0.5' },
+        }
+      },
       fontFamily: {
         sans: [
           'system-ui',
